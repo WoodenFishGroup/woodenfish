e4d2bcf636a2963b5fd51176b78ae475b280abea
Yizhe Tang <yizhe.tang@hulu.com>
2014-03-04 13:39:59 +0800

    add post is_delete field

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ require 'digest/md5'
 
 class HomeController < ApplicationController
   def index
-    @posts = Post.where("id > 0").paginate(:page => params[:page], :per_page => 20).order("id DESC")
+    @posts = Post.where("is_deleted=0").paginate(:page => params[:page], :per_page => 20).order("id DESC")
   end
 
   def health_check
