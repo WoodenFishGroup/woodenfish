2e3554dc5072d6902e014d54ed9b22026ec083bf
Jia Cao <caojia@hulu.com>
2014-03-10 16:46:14 +0800

    change the from header

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,12 @@
 class NotificationMailer < ActionMailer::Base
-  default from: Rails.configuration.notify_from_alias
+  #default from: Rails.configuration.notify_from_alias
 
   def new_post_notify(post)
     users = User.find_new_post_notified_users
     aliases = users.map{|user| user.email}
     subject = "[Woodenfish] #{post.subject}"
     @post = post
-    mail(bcc: aliases, subject: subject, message_id: post.source_id)
+    mail(from: format_from(post.user), bcc: aliases, subject: subject, message_id: post.source_id)
   end
 
   def new_comment_notify(comment)
@@ -14,6 +14,11 @@ class NotificationMailer < ActionMailer::Base
     aliases = users.map{|user| user.email}
     subject = "Re: [Woodenfish] #{comment.post.subject}"
     @comment = comment
-    mail(bcc: aliases, subject: subject, message_id: comment.source_id)
+    mail(from: format_from(post.user), bcc: aliases, subject: subject, message_id: comment.source_id)
+  end
+
+  private
+  def format_from(user)
+    %Q{"#{user.name} (WoodenFish)" <#{Rails.configuration.notify_from_alias}>}
   end
 end
